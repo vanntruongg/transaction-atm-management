647ef093380ff65185aad6cd1320072c8c5472dc
Van Truong <truongtran.lv2019@gmail.com>
2024-01-18 00:21:36 +0700

    add map

--- resources/views/components/common/head.blade.php
+++ resources/views/components/common/head.blade.php
@@ -7,6 +7,7 @@
         <title>@yield('title')</title>
 
         @vite('resources/css/app.css')
+        @vite('resources/js/index.js')
     </head>
     <body class="antialiased">
       <div class="main">
