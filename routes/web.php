ba03ff25b7cce89171952af57938d5c35c470314
DucDong19 <voducdong.nvkg123@gmail.com>
2024-02-29 08:45:04 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+// Route::get('product/search-advance', 'ProductController@search');
+// Route::post('product/search-advance', 'ProductController@getProductSearch');
+
+// Route::get('/search', 'HomeController@search');
+// Route::post('/search', 'HomeController@searchFullText')->name('search');
+
