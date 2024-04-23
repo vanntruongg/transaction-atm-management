7d046263aa4bd685d1122b40b16ff01104497434
Vo Duy <duyttbt@gmail.com>
2024-04-24 00:02:24 +0700

    update lai lan 2

--- resources/js/index.js
+++ resources/js/index.js
@@ -26,31 +26,6 @@ L.marker([lat, lng]).addTo(map).bindPopup("Đại học Cần Thơ khu II").open
 var geocoder = L.Control.Geocoder.nominatim();
 L.Control.geocoder({ geocoder }).addTo(map);
 
-// menu sidebar
-// const menuSidebar = document.getElementById("menu-sidebar");
-// const sidebar = document.getElementById("sidebar");
-// const divMap = document.getElementById("divMap");
-// const openSidebar = document.getElementById("openSidebar");
-// const closeSidebar = document.getElementById("closeSidebar");
 
-// menuSidebar.addEventListener("click", () => {
-//     menuSidebar.classList.toggle("active");
-
-//     if (menuSidebar.classList.contains("active")) {
-//         closeSidebar.classList.remove("hidden");
-//         openSidebar.classList.add("hidden");
-//         sidebar.classList.remove("w-[5%]");
-//         divMap.classList.remove("w-[95%]");
-//         sidebar.classList.add("w-[20%]");
-//         divMap.classList.add("w-[80%]");
-//     } else {
-//         closeSidebar.classList.add("hidden");
-//         openSidebar.classList.remove("hidden");
-//         sidebar.classList.add("w-[5%]");
-//         divMap.classList.add("w-[95%]");
-//         sidebar.classList.remove("w-[20%]");
-//         divMap.classList.remove("w-[80%]");
-//     }
-// });
 
 export default map;
